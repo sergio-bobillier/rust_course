503bde55be23c430c8d32f9ed88600862913383a
Sergio Bobillier <sergio.bobillier@gmail.com>
2023-09-03 21:10:04 +0200

    Optimize the parameters handling in Crop (exercise Z)

--- exercise/z_final_project/src/command_factories/crop_factory.rs
+++ exercise/z_final_project/src/command_factories/crop_factory.rs
@@ -2,7 +2,9 @@ use std::collections::VecDeque;
 
 use crate::processing_commands::Crop;
 use crate::geometry::Geometry;
+use super::parameter_fetcher::fetch_and_parse;
 use super::parameter_fetcher::parameters_left;
+use super::parameter_fetcher::require_and_parse;
 
 pub struct CropFactory {
     parameters: VecDeque<String>
@@ -14,10 +16,19 @@ impl CropFactory {
     }
 
     pub fn create(&mut self) -> Result<Crop, String> {
-        let left: u32 = self.required("left")?;
-        let top: u32 = self.required("top")?;
-        let width: Option<u32> = self.consume("width")?;
-        let height: Option<u32> = self.consume("height")?;
+        let command = "crop";
+
+        let parameter = "left";
+        let left = require_and_parse(&mut self.parameters, parameter, command)?;
+
+        let parameter = "top";
+        let top = require_and_parse(&mut self.parameters, parameter, command)?;
+
+        let parameter = "width";
+        let width = fetch_and_parse(&mut self.parameters, parameter, command)?;
+
+        let parameter = "height";
+        let height = fetch_and_parse(&mut self.parameters, parameter, command)?;
 
         let geometry = Geometry::new(
             left, top, width, height
@@ -25,42 +36,5 @@ impl CropFactory {
 
         parameters_left(&self.parameters, "crop")?;
         Ok(Crop::new(geometry))
-    }
-
-    fn required(&mut self, name: &str) -> Result<u32, String> {
-        let value = self.consume(name)?;
-
-        match value {
-            Some(value) => {
-                Ok(value)
-            },
-            None => {
-                Err(format!("Missing required parameter {} for crop", name))
-            }
-        }
-    }
-
-    fn consume(&mut self, name: &str) -> Result<Option<u32>, String> {
-        let parameter = self.parameters.pop_front();
-
-        match parameter {
-            Some(string) => {
-                if string == "-" { return Ok(None) }
-
-                let result = string.parse::<u32>();
-
-                match result {
-                    Ok(value) => {
-                        Ok(Some(value))
-                    }
-                    Err(_e) => {
-                        Err(format!("Cannot parse {} as {} for --crop", string, name))
-                    }
-                }
-            }
-            None => {
-                Ok(None)
-            }
-        }
     }
 }
